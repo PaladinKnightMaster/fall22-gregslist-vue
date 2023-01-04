d60347c9064aec9cfd6628940b41964029f7b687
PaladinKnightMaster <paladinknight.master@gmail.com>
2023-01-04 13:21:18 -0700

    Getting houses and jobs into the site

--- src/components/ClassifiedForm.vue
+++ src/components/ClassifiedForm.vue
@@ -44,6 +44,54 @@
       <h1>TODO</h1>
     </div>
 
+    <div class="house-form text-dark" v-if="placeholder == true">
+      <form onsubmit="app.housesController.handleSubmit()">
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="bedrooms" required minlength="3" value="${editable.bedrooms}">
+          <label for="bedrooms">Bedrooms</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="bathrooms" value="${editable.bathrooms}">
+          <label for="bathrooms">Bathrooms</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="levels" required min="1" value="${editable.levels}">
+          <label for="levels">Levels</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="url" class="form-control" name="imgUrl" value="${editable.imgUrl}">
+          <label for="imgUrl">Picture!</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="year" required max="9999" value="${editable.year}">
+          <label for="year">Year</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="price" required min="0" value="${editable.price}">
+          <label for="price">Price</label>
+        </div>
+
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Describe your Listing"
+            name="description">${editable.description}</textarea>
+          <label for="description">Description</label>
+        </div>
+
+        < class="d-flex my-4 gap-5 align-items-center">
+          <button class="btn" type="reset">Cancel</button>
+          <button class="btn btn-primary" type="submit">${
+            editable.id ? 'Save Changes' : 'Create'
+            } </button>
+      </form>
+    </div>
+
+
 
     <div class="my-3" v-if="editable.listingType">
       <button class="btn btn-success" type="submit">SEND IT</button>
